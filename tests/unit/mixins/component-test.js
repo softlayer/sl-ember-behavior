818e01d20393a87a9c83df64bc8d25108ba3e32d
Jonathan Davidson <jcdavidson@softlayer.com>
2015-08-05 16:27:34 -0500

    Closes #80 Add more testing around 'mixins/component' showContent computed property

--- tests/unit/mixins/component-test.js
+++ tests/unit/mixins/component-test.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 import ComponentMixin from 'sl-ember-behavior/mixins/component';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 let AugmentedObject;
+let isViewableStub;
 
 module( 'Unit | Mixin | component', {
     beforeEach() {
-        AugmentedObject = Ember.Object.extend( ComponentMixin );
+        isViewableStub = sinon.stub().returns( true );
+
+        AugmentedObject = Ember.Object.extend( ComponentMixin, {
+            isViewable: isViewableStub
+        });
     }
 });
 
@@ -21,10 +27,111 @@ test( 'The correct service is being injected into the component', function( asse
 });
 
 test( 'Assert is thrown when isViewable() is not implemented on the derived class', function( assert ) {
+    AugmentedObject = Ember.Object.extend( ComponentMixin );
+
     const subject = AugmentedObject.create();
 
     assert.throws(
         subject.isViewable,
         'Assertion was thrown'
+    );
+});
+
+test( 'showContent() computed property throws assertion when `possible` property is not a boolean', function( assert ) {
+    const subject = AugmentedObject.create();
+    const callShowContent = () => subject.get( 'showContent' );
+
+    // Array
+    subject.set( 'possible', [] );
+
+    assert.throws(
+        callShowContent,
+        'The `possible` property is an Array'
+    );
+
+    // Boolean
+    subject.set( 'possible', false );
+
+    let assertionThrown = false;
+
+    try {
+        callShowContent();
+    } catch( error ) {
+        assertionThrown = true;
+    }
+
+    assert.ok(
+        !assertionThrown,
+        'The possible property is a Boolean'
+    );
+
+    // Function
+    subject.set( 'possible', () => {} );
+
+    assert.throws(
+        callShowContent,
+        'The possible property is a Function'
+    );
+
+    // Instance
+    subject.set( 'possible', Ember.Object.create() );
+
+    assert.throws(
+        callShowContent,
+        'The possible property is an Instance'
+    );
+
+    // Null
+    subject.set( 'possible', null );
+
+    assert.throws(
+        callShowContent,
+        'The `possible` property is Null'
+    );
+
+    // Number
+    subject.set( 'possible', 123 );
+
+    assert.throws(
+        callShowContent,
+        'The `possible` property is a Number'
+    );
+
+    // Object
+    subject.set( 'possible', {} );
+
+    assert.throws(
+        callShowContent,
+        'The `possible` property is an Object'
+    );
+
+    // Undefined
+    subject.set( 'possible', undefined );
+
+    assert.throws(
+        callShowContent,
+        'The `possible` property is Undefined'
+    );
+});
+
+test( 'showContent() computed property returns the value the isViewable method returns', function( assert ) {
+    let subject = AugmentedObject.create();
+
+    assert.equal(
+        subject.get( 'showContent' ),
+        true,
+        'Is viewable returns true'
+    );
+
+    AugmentedObject.reopen({
+        isViewable: sinon.stub().returns( false )
+    });
+
+    subject = AugmentedObject.create();
+
+    assert.equal(
+        subject.get( 'showContent' ),
+        false,
+        'Is viewable returns false'
     );
 });
