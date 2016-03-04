8a55edfd19ed2a1dbc1c5d5ca334eaf613de3db7
Jeremy Brown <jeremy@notmessenger.com>
2016-03-03 20:32:01 -0600

    Closes softlayer/sl-ember-behavior#156

---
+++ testem.js
@@ -0,0 +1,18 @@
+/* jshint node:true*/
+module.exports = {
+    'framework': 'qunit',
+    'test_page': 'tests/index.html?hidepassed&nojshint',
+    'disable_watching': true,
+    'launch_in_ci': [
+        'PhantomJS'
+    ],
+    'launch_in_dev': [
+        'PhantomJS',
+        'Chrome',
+        'IE10',
+        'IE11',
+        'Firefox',
+        'Safari',
+        'Opera'
+    ]
+};
